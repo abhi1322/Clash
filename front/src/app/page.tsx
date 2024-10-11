8d902d67719355998ae0fe11d364e6095d2da967
Abhishek Kumar <abhi1322jii@gmail.com>
2024-10-11 17:23:47 +0530

    feat: prisma setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <>frontend setup</>;
+  return (
+    <>
+      frontend setup
+      <Button>S</Button>
+    </>
+  );
 }
