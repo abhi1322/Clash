9fb6944547c7fe98f33463e1edad8f0dc940f8a0
Abhishek Kumar <abhi1322jii@gmail.com>
2024-10-16 15:19:47 +0530

    feat: register route setup

--- server/dist/routes/authRoute.js
+++ server/dist/routes/authRoute.js
@@ -1,14 +1,33 @@
 import { Router } from "express";
 import { registerSchema } from "../validation/authValidation.js";
-const AuthRoutes = Router();
-AuthRoutes.post("/", async (req, res, next) => {
+import { ZodError } from "zod";
+import { formatError } from "../helper.js";
+import prisma from "../config/database.js";
+const routes = Router();
+routes.post("/register", async (req, res) => {
     try {
         const body = req.body;
         const payload = registerSchema.parse(body);
         res.json(payload);
+        let user = await prisma.user.findUnique({
+            where: {
+                email: payload.email,
+            },
+        });
+        if (user) {
+            return res
+                .status(422)
+                .json({ message: "Email already exists, please use a different one." });
+        }
     }
     catch (error) {
-        res.status(422).json(error);
+        if (error instanceof ZodError) {
+            const errors = formatError(error);
+            return res.status(422).json({ message: "Invalid data", errors });
+        }
+        return res
+            .status(500)
+            .json({ message: "Something went wrong, please try again." });
     }
 });
-export default AuthRoutes;
+export default routes;
--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,19 +1,41 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { registerSchema } from "../validation/authValidation.js";
+import { ZodError } from "zod";
+import { formatError } from "../helper.js";
+import prisma from "../config/database.js";
 
-const AuthRoutes = Router();
+const routes = Router();
 
-AuthRoutes.post(
-  "/",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const body = req.body;
-      const payload = registerSchema.parse(body);
-      res.json(payload);
-    } catch (error) {
-      res.status(422).json(error);
+routes.post("/register", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const payload = registerSchema.parse(body);
+    res.json(payload);
+
+    let user = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+
+    if (user) {
+      return res
+        .status(422)
+        .json({ message: "Email already exists, please use a different one." });
     }
-  }
-);
 
-export default AuthRoutes;
+    
+
+
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const errors = formatError(error);
+      return res.status(422).json({ message: "Invalid data", errors });
+    }
+    return res
+      .status(500)
+      .json({ message: "Something went wrong, please try again." });
+  }
+});
+
+export default routes;
